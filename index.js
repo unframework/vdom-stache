87246fd08ae43fca5e748da53cfaf48a30103b25
Nick Matantsev <nick.m@myplanetdigital.com>
2016-09-28 18:32:13 -0400

    Implement DOM stitching in cache handler

--- index.js
+++ index.js
@@ -14,19 +14,80 @@ document.onclick = function () {
 var container = document.createElement('div');
 document.body.appendChild(container);
 
+var domStage = document.createElement('div');
+
 function cacheHandler() {
     return function (text, render) {
-        var html = render(text);
+        var parentSnippetList = this._renderSnippetList;
+        var currentSnippetList = [];
 
-        return html;
+        // render template, collecting cacheable child snippets
+        this._renderSnippetList = currentSnippetList;
+        var html = render(text);
+        this._renderSnippetList = parentSnippetList;
+
+        // instantiate resulting HTML in a staging area
+        domStage.innerHTML = html;
+
+        var domSet = Array.apply(null, new Array(domStage.childNodes.length)).map(function (x, index) {
+            return domStage.childNodes[index];
+        });
+
+        // unlink staged nodes
+        domStage.innerHTML = '';
+
+        // stitch together resulting DOM by replacing child snippet markers
+        var snippetMarkerNodeList = [];
+
+        domSet.forEach(function (domNode) {
+            var walker = document.createTreeWalker(domNode, NodeFilter.SHOW_COMMENT, {
+                acceptNode: function (node) {
+                    return node.nodeValue === '$cache$' ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT;
+                }
+            }, false);
+
+            while(walker.nextNode()) {
+                snippetMarkerNodeList.push(walker.currentNode);
+            }
+        });
+
+        snippetMarkerNodeList.forEach(function (snippetMarkerNode, index) {
+            currentSnippetList[index].forEach(function (contentNode) {
+                snippetMarkerNode.parentNode.insertBefore(contentNode, snippetMarkerNode);
+            });
+
+            snippetMarkerNode.parentNode.removeChild(snippetMarkerNode);
+        });
+
+        if (parentSnippetList) {
+            // return a stub for later stitching by parent
+            parentSnippetList.push(domSet);
+
+            return '<!--$cache$-->';
+        } else {
+            // top-level result
+            cacheHandler.lastRootDomSet = domSet;
+
+            return '<!--$cacheRoot$-->';
+        }
     }
 }
 
 function render() {
-    container.innerHTML = Mustache.render(template, {
+    var textOutput = Mustache.render(template, {
         clickCount: clickCount,
 
         cache: cacheHandler
+    });
+
+    // present the root DOM in the container
+    if (textOutput !== '<!--$cacheRoot$-->') {
+        throw new Error('cache tag must be top-level');
+    }
+
+    container.innerHTML = '';
+    cacheHandler.lastRootDomSet.forEach(function (node) {
+        container.appendChild(node);
     });
 }
 
